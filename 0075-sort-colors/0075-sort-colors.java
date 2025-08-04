19b45620710041e6cf20190bdc7ec02602b72a40
Ashish Namdeo <ashishnamdev14@gmail.com>
2025-08-04 01:06:03 -0700

    Time: 2 ms (13.22%), Space: 41.8 MB (92.98%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int minIndex = i; 
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j] < nums[minIndex]){
+                    minIndex =j;
+                }
+            }
+         // Swaping elements
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+            }
+    }
+}
