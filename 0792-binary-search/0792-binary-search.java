21e84aa0b4e28b106e8a1f9d407f38b477320158
Ashish Namdeo <ashishnamdev14@gmail.com>
2025-08-25 12:33:41 -0700

    Time: 0 ms (100%), Space: 46.1 MB (14.29%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,10 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                return i;           
+        //Optimized Approach
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                start = mid+1;
+            }else if(nums[mid] > target){
+                end = mid-1;
             }
         }
+        //Brute Force Approach
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] == target){
+        //         return i;           
+        //     }
+        // }
         return -1;
     }
 }
