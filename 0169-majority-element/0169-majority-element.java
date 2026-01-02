fa59d048221a1f7b276deba8240b9aa10c77f933
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-01-02 12:23:26 -0800

    Time: 1 ms (99.82%), Space: 55.8 MB (30.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int count =0;
+        int res=0;
        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-
-        int maxfreq =0;
-        int majorityElement = nums[0]; 
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            if(entry.getValue() >  maxfreq){
-                maxfreq = entry.getValue();
-                majorityElement = entry.getKey();
+       for(int i=0;i<nums.length;i++){
+            if (res == nums[i]) {
+                count++;
+            }else if(count == 0) {
+                res = nums[i];
+                count = 1;
+            }else {
+                count--;
             }
-        }
-        return majorityElement;
+       }
+
+       return res;
+
+        //Brute Force Solution
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+
+        // int maxfreq =0;
+        // int majorityElement = nums[0]; 
+        // for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+        //     if(entry.getValue() >  maxfreq){
+        //         maxfreq = entry.getValue();
+        //         majorityElement = entry.getKey();
+        //     }
+        // }
+        // return majorityElement;
     }
 }
