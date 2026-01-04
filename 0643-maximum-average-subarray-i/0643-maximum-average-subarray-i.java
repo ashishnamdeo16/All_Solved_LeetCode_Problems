1d91df4432a9242c20cbe0568830a14303643464
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-01-04 12:21:25 -0800

    Time: 5 ms (44.58%), Space: 56.3 MB (99.92%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,33 +1,27 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-    //Intializing the pointers and variables    
-    int l=0;
-    int r=0;
-    int sum = 0;
-    double maxAvg = Double.NEGATIVE_INFINITY;   
-
-    //Starting the Conditions for Edge Case
-    if(k == nums.length){
-        for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-        }
-        return (double)(sum/k);
-    }else{
-        //Actual Loop for Fixing the Window
-        while(r<nums.length){
-            sum += nums[r];
-            //Condition which will increament the pointer till window size is not Fixed
-            if(r-l+1 < k){
-                r++;
-            }else if(r-l+1 == k){
-                //Window is fixed for k size
-                maxAvg = Math.max((double)sum/k,maxAvg);
-                sum -= nums[l];
-                l++;
-                r++;
+        int i=0;
+        int j=0;
+        int sum = 0;
+        double maxAvg = Double.NEGATIVE_INFINITY;
+        if(k == nums.length){
+            for(int l=0;l<nums.length;l++){
+                sum += nums[l];
+            }
+            return (double)sum/k;
+        }else{
+            while(j<nums.length){
+            sum += nums[j];
+            if(j-i+1 < k){
+                j++;
+            }else if(j-i+1 == k){
+                maxAvg = Math.max(maxAvg,(double)sum/k);
+                sum -= nums[i];
+                i++;
+                j++;
             }
         }
         return maxAvg;
-    }
+        }
     }
 }
