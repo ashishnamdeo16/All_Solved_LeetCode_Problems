7877fb8c86b9d08d9e56d5ea6c0c5b479f9bdde2
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-01-18 18:24:35 -0800

    Time: 0 ms (100%), Space: 45.4 MB (83.33%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return findAllPermutationof(nums);
+    }
+
+    public static List<List<Integer>> findAllPermutationof(int[] arr){
+        ArrayList<List<Integer>> finalList = new ArrayList<>();
+        recurList(0,arr,finalList);
+        return finalList;
+    }
+
+    public static void recurList(int start,int[] nums,List<List<Integer>> finalList){
+        if(start == nums.length){
+            List<Integer> list = new ArrayList<>();
+            for (int x : nums) list.add(x);
+            finalList.add(list);
+            return;
+        }
+        for(int i=start;i<nums.length;i++){
+            swap(nums,start,i);
+            recurList(start+1,nums,finalList);
+            swap(nums,start,i);
+        }
+
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
