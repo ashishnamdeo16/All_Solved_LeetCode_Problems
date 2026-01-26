677404ebccd4f48573f477f55298b9f574c66c46
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-01-26 10:47:56 -0800

    Time: 1 ms (87.95%), Space: 44.3 MB (48.83%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        return computeSubSets(nums);
+    List<List<Integer>> finalList = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    findAllSubsets(nums,0,temp,finalList);
+    return finalList;
     }
 
-
-    public static List<List<Integer>> computeSubSets(int[] nums){
-        List<List<Integer>> finalList = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        finalSubset(nums,0,temp,finalList);
-        return finalList;
-    }
-
-    public static void finalSubset(int[] nums, int start,  List<Integer> temp,List<List<Integer>>  finalList){
-        finalList.add(new ArrayList<>(temp));
-        for(int i=start;i<nums.length;i++){
-            temp.add(nums[i]);
-            finalSubset(nums,i+1,temp,finalList);
-            temp.remove(temp.size() - 1);
+    public static void findAllSubsets(int[] nums,int index, List<Integer>temp, List<List<Integer>> finalList){
+        if(index == nums.length){
+            finalList.add(new ArrayList<>(temp));
+            return;
         }
+            findAllSubsets(nums,index+1,temp,finalList);
+            temp.add(nums[index]);
+            findAllSubsets(nums,index+1,temp,finalList);
+            temp.remove(temp.size()-1);
     }
 }
