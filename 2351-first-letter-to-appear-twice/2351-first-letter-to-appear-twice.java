d7ba87045a02f3a12b1bc79dcebb562ac8c46ff3
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-01-31 18:47:57 -0800

    Time: 0 ms (100%), Space: 43.1 MB (9.45%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,19 +1,21 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        // int[] freq = new int[26];
-        // for(char c : s.toCharArray()){
-        //     if(freq[c - 'a'] < 1){
-        //       freq[c - 'a']++;
-        //     }
-        // }
-        Map<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         char finalRep = '\0';
-        for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
-                return s.charAt(i);
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
+            if(freq[c - 'a'] > 1){
+             return c;
             }
-            map.put(s.charAt(i),1);
         }
+        // Map<Character,Integer> map = new HashMap<>();
+        // char finalRep = '\0';
+        // for(int i=0;i<s.length();i++){
+        //     if(map.containsKey(s.charAt(i))){
+        //         return s.charAt(i);
+        //     }
+        //     map.put(s.charAt(i),1);
+        // }
         return finalRep;
     }
 }
