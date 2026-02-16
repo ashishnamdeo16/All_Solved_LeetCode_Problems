58082f16fb35eab4bfb2a8c5669ba2718ba834b1
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-02-15 20:31:22 -0800

    Time: 4 ms (44.11%), Space: 47.6 MB (43.05%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,22 +1,48 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int l = 0 ;
-        int r = nums.length - 1;
-        while(l<=r){
-            if(l == r){
-                nums[l] = nums[l] * nums[l];
+        int l = 0;
+
+        List<Integer> neg = new ArrayList<>();
+        List<Integer> pos = new ArrayList<>();
+
+        while(l<nums.length){
+            if(nums[l] < 0){
+                neg.add(nums[l] * nums[l]);
             }else{
-                nums[l] = nums[l] * nums[l];
-                nums[r] = nums[r] * nums[r];
+                pos.add(nums[l] * nums[l]);
             }
             l++;
-            r--;
         }
 
+        int i = neg.size()-1;
+        int j = 0;
+        int o = 0;
+
+        while(i >= 0 && j < pos.size()){
+            if(neg.get(i) <= pos.get(j)){
+                nums[o++] = neg.get(i);
+                i--;
+            }else if(neg.get(i) > pos.get(j)){
+                nums[o++] = pos.get(j);
+                j++;
+            }
+        }
+
+        while(i >=0 ){
+             nums[o++] = neg.get(i);
+             i--;
+        }
+
+        while( j < pos.size()){
+              nums[o++] = pos.get(j);
+              j++;
+        }
+
+        //Brute Force
         // for(int i=0;i<nums.length;i++){
         //     nums[i] = nums[i] * nums[i];
         // }
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         return nums;
  }
 }
