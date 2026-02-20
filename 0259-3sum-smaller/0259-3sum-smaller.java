6f5932b0aff7fc40d1e177cb4eeac33d7c821de4
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-02-20 13:29:11 -0800

    Time: 22 ms (12.47%), Space: 45.5 MB (32.1%) - LeetHub

---
+++ 0259-3sum-smaller/0259-3sum-smaller.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int l = i+1;
+            int r = nums.length-1;
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum < target){
+                    count = count + (r-l);
+                    l++;
+                }else if(sum >= target){
+                    r--;
+                }
+            }
+        }
+        return count;
+    }
+}
