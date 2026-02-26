cbcd19565b35ddab22ce513cb1fb6f3c5867e545
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-02-26 14:32:22 -0800

    Time: 38 ms (12.36%), Space: 79.3 MB (94.2%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,17 +1,38 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-    HashMap<Integer,Integer> arr = new HashMap<>();
-    int i=0;
-    while(i<nums.length){
-        arr.put(nums[i],arr.getOrDefault(nums[i],0)+1);
-        i++;
-    }
+    public int findDuplicate(int[] nums) {  
 
-    for(Map.Entry<Integer, Integer> entry : arr.entrySet()){
-        if(entry.getValue() != 1){
-            return entry.getKey();
+        Arrays.sort(nums);
+        int l = 0;
+        int r = l+1;
+        while(r<nums.length){
+            if(nums[l] == nums[r]){
+                return nums[l];
+            }
+            l++;
+            r++;
         }
-    }
-    return -1;
+
+        return -1;
+
+
+        // int res = 0;
+        // for(int i :nums){
+        //     res^=i;
+        // }
+        //  return res;
+
+    // HashMap<Integer,Integer> arr = new HashMap<>();
+    // int i=0;
+    // while(i<nums.length){
+    //     arr.put(nums[i],arr.getOrDefault(nums[i],0)+1);
+    //     i++;
+    // }
+
+    // for(Map.Entry<Integer, Integer> entry : arr.entrySet()){
+    //     if(entry.getValue() != 1){
+    //         return entry.getKey();
+    //     }
+    // }
+
     }
 }
