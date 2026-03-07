90d59d5e1abe0fcce165c0b18f8507a2a23d804a
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-03-07 09:00:53 -0800

    Time: 78 ms (15.42%), Space: 97.6 MB (98.47%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Pair> stack = new Stack<>();
-        int[] ans = new int[temperatures.length];
-        for(int i = temperatures.length - 1;i>=0;i--){
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        ans[n-1] = 0;
+        stack.push(new Pair(temperatures[n-1],n-1));
+        for(int i=n-2;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek().temp <= temperatures[i]){
+                stack.pop();
+            }
             if(stack.isEmpty()){
                 ans[i] = 0;
-            }else if( temperatures[i] >= stack.peek().temp){
-                while(!stack.isEmpty() && temperatures[i] >= stack.peek().temp){
-                    stack.pop();
-                }
-                if(stack.isEmpty()){
-                    ans[i] = 0;
-                }else{
-                    ans[i] = stack.peek().index - i;
-                }
-            }else if(!stack.isEmpty() && temperatures[i] < stack.peek().temp){
+            }else{
                 ans[i] = stack.peek().index - i;
             }
             stack.push(new Pair(temperatures[i],i));
@@ -22,10 +19,10 @@ class Solution {
         return ans;
     }
     public class Pair{
+        int index ;
         int temp;
-        int index;
-        public Pair(int temp, int index){
-            this.temp = temp; 
+        public Pair(int temp,int index){
+            this.temp = temp;
             this.index = index;
         }
     }
