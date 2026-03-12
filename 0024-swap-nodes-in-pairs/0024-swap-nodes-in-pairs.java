4c4306bfa2c509c8a1bddb8d176cde3618dbce2a
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-03-11 19:19:01 -0700

    Time: 0 ms (100%), Space: 42.8 MB (95.86%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -1,37 +1,72 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode swapPairs(ListNode head) {
         if(head == null) return head;
-        if(head != null && head.next == null) return head;
-        ListNode curr = head;
-        ListNode prev = null;
-        head = head.next; 
-
-        while(curr != null && curr.next != null){
-            ListNode temp = curr;
-            ListNode ref = curr.next;
-            ListNode ref2 = curr.next.next;
-            curr = curr.next.next;
-            ref.next = temp;
-            temp.next = ref2;
-
-            if(prev != null){
-                prev.next = ref;
-            }
-
-            prev = temp;
-            curr = ref2;
+        ListNode left = head;
+        ListNode right = head;
+        ListNode prevLeft = null;
+        ListNode res = null;
+        int size = 2;
+        while(true){
+            right = left;    
+            for(int i=0;i<size-1;i++){
+                if(right == null){
+                    break;
+                }
+                right = right.next;
+            }          
+            if(right != null){
+                ListNode nextLeft = right.next;
+                reverse(left,2);
+                if(res == null){
+                    res = right;
+                }
+                if(prevLeft != null){
+                    prevLeft.next = right;
+                }
+                prevLeft = left;
+                left = nextLeft;
+            }else{
+                if(prevLeft != null){
+                    prevLeft.next = left;
+                }
+                if(res == null){
+                    res = left;
+                }
+                break;
+            }   
         }
+
+        // if(head != null && head.next == null) return head;
+        // ListNode curr = head;
+        // ListNode prev = null;
+        // head = head.next; 
+
+        // while(curr != null && curr.next != null){
+        //     ListNode temp = curr;
+        //     ListNode ref = curr.next;
+        //     ListNode ref2 = curr.next.next;
+        //     curr = curr.next.next;
+        //     ref.next = temp;
+        //     temp.next = ref2;
+
+        //     if(prev != null){
+        //         prev.next = ref;
+        //     }
+
+        //     prev = temp;
+        //     curr = ref2;
+        // }
         
-        return head;
+        return res;
+    }
+    public void reverse(ListNode left,int size){
+        ListNode curr = left;
+        ListNode temp = null;
+        for(int i=0;i<size;i++){
+            ListNode ref = curr.next;
+            curr.next = temp;
+            temp = curr;
+            curr = ref;
+        }
     }
 }
