ef1afacd5524ebee66211c3184683f2d27217fed
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-03-27 12:51:34 -0700

    Time: 30 ms (64.77%), Space: 58.8 MB (23.33%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,13 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) ->
-            {
-                if(a.dist != b.dist){
-                    return b.dist - a.dist;
-                }
-                return b.index - a.index;
-            }
-        );    
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.dist - a.dist);    
 
         for(int i =0 ;i<points.length;i++){
             int dist = points[i][0] * points[i][0]  + points[i][1]  * points[i][1];
