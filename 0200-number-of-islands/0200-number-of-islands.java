754365518268ee20e8e250e2524b2fd0f60b4959
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-10 13:31:49 -0700

    Time: 4 ms (43.52%), Space: 52.3 MB (63.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,48 @@
 class Solution {
-    int count = 0;
-    int[] px = {0,-1,0,1};
-    int[] py = {-1,0,1,0};
+    int[] px = {0,1,0,-1};
+    int[] py = {1,0,-1,0};
 
     public int numIslands(char[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        for(int i=0;i<grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                if(grid[i][j] == '1'){
-                    dfs(i,j,grid,r,c);
-                    count++;
+        int res = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+          Arrays.fill(visited[i], false);
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1' && visited[i][j] == false){
+                    dfs(grid,m,n,i,j,visited);
+                    res++;
                 }
             }
         }
-
-        return count;
+        
+        return res;
     }
 
-    public void dfs(int i,int j,char[][] grid,int r,int c){
-        if(i<0 || j <0 || i>=r || j >= c || grid[i][j] != '1'){
-            return;
-        }
-
-        grid[i][j] = '2';
+    public void dfs(char[][] grid,int m,int n,int i,int j,boolean[][] visited){
+        
+        visited[i][j] = true;
 
         for(int k=0;k<4;k++){
-            int ii = i + px[k];
-            int jj = j + py[k];
-            dfs(ii,jj,grid,r,c);
+            int x = px[k] + i;
+            int y = py[k] + j;
+            if(isValid(x,y,m,n) && !visited[x][y] && grid[x][y] == '1'){
+                dfs(grid,m,n,x,y,visited);
+            }
         }
+
+    }   
+
+    public boolean isValid(int i,int j,int m,int n){
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return false;
+        }
+        return true;
     }
 }
