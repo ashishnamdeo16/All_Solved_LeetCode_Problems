5e82237a514e0f09e0d225ff9f40c43429ac641a
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-10 00:48:27 -0700

    Time: 8 ms (17.75%), Space: 46.5 MB (42.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,13 +4,17 @@ class Solution {
         int maxLength = 0;
         int l = 0;
         int r = 0;
+        StringBuilder sb = new StringBuilder();
+
         while(r < s.length()){
             char ch = s.charAt(r);
             if(!set.contains(ch)){
+                sb.append(ch);
                 set.add(ch);
-                maxLength = Math.max(maxLength, set.size());
+                maxLength = Math.max(maxLength, sb.length());
                 r++;
             }else{
+                sb.deleteCharAt(0);
                 set.remove(s.charAt(l));
                 l++;
             }
