2d2c116bf25270f951946037aa0df5ac0e517c0d
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-18 12:44:00 -0700

    Time: 39 ms (9.46%), Space: 46.2 MB (7%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -7,8 +7,8 @@ class Solution {
     }
 
     public int fibDp(int n,int[] dp){
-      if(dp[n] != -1) return dp[n];
       if(n < 2 ) return n;
+      if(dp[n] != -1) return dp[n];
       dp[n] = fib(n-1) + fib(n-2);
       return dp[n];
     }
