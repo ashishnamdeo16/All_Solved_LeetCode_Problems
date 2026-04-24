708b960ecdac34221008bc8be8b2b2c4b36d4169
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-24 02:38:15 -0700

    Time: 3 ms (61.61%), Space: 43.6 MB (60.1%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(!stack.isEmpty() && Math.abs(stack.peek() - ch) == 32){
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+         
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            char c = stack.pop();
+            sb.append(c);
+        }
+
+        sb.reverse();
+        return sb.toString();
+    }
+}
