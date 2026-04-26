acb1b2df89161520673de276cc97528e44857783
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-26 03:45:55 -0700

    Time: 1 ms (36.56%), Space: 44.1 MB (79.43%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(int i=0;i<words.length;i++){
+            if(words[i].contains(pref) && checkIfPrefix(words[i],pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean checkIfPrefix(String word,String pref){
+        for(int i=0;i<pref.length();i++){
+            if(word.charAt(i) != pref.charAt(i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
