2ff0e2ea938db45e919055bfc902b2b1c805ba53
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-27 02:47:01 -0700

    Time: 1 ms (37.71%), Space: 45.5 MB (93.29%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -5,23 +5,31 @@ class Solution {
             int l = 0;
             int r = image[i].length-1;
 
-            while(l<r){
+            while(l<=r){
                 int temp = image[i][l];
                 image[i][l] = image[i][r];
                 image[i][r] = temp;
+
+                if (l == r) {
+                image[i][l] ^= 1;  // invert once
+                } else {
+                    image[i][l] ^= 1;
+                    image[i][r] ^= 1;
+                }
+
                 l++;
                 r--;
             }
 
-            int k=0;
-            while(k < image[i].length){
-                if(image[i][k] == 0){
-                    image[i][k] = 1;
-                }else{
-                     image[i][k] = 0;
-                }
-                k++;
-            }
+            // int k=0;
+            // while(k < image[i].length){
+            //     if(image[i][k] == 0){
+            //         image[i][k] = 1;
+            //     }else{
+            //          image[i][k] = 0;
+            //     }
+            //     k++;
+            // }
         }
 
         return  image;
