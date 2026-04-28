c7f2a80cab31c5b0321187bb52140bb9bd9a24ee
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-04-28 14:13:19 -0700

    Time: 5 ms (24.65%), Space: 97.7 MB (96.64%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.java
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -33,7 +33,7 @@ class Solution {
             }
 
             if(l == r){
-            if(Math.max(aliceW, bobW) < plants[l]) {
+               if(Math.max(aliceW, bobW) < plants[l]) {
                 count++;
                 }
             }
@@ -43,20 +43,4 @@ class Solution {
 
     }
 }
-
-//Alice Capcity = capacityA
-//Bob Capacity = capacity B
-//n = no of Plants 
-//index = plant 
-//plants[index] = plants location
-//[2,2,3,3,7]
-//a = 7 b = 7
-
-// l = 0 and r = 4
-// l++ = 1 => a = 5
-// r-- = 3 => b = 0
-// l = 1 and r = 3 
-// l++ = 2 => a = 2
-// r-- = 3 => b = 0 -  count = 1
-
 
