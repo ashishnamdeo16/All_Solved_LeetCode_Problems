0d93c259e4b81b80289f1335fe829d1cf37d2efe
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-05-03 11:24:26 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,37 @@
 class Solution {
+
     public int largestRectangleArea(int[] heights) {
-       return maxArea(allAreas(heights,width(NearestSmallerToLeft(heights),NearestSmallerToRight(heights))));
+        return maxArea(allAreas(heights,width(NSL(heights),NSR(heights))));
     }
 
-    public int[] NearestSmallerToLeft(int[] heights){
-        int[] arr = new int[heights.length];
+    public int[] NSL(int[] heights){
+        int[] ans = new int[heights.length];
         Stack<int[]> stack = new Stack<>();
-        
-        stack.push(new int[]{heights[0],0});
-        arr[0] = -1;
 
-        for(int i=1;i<heights.length;i++){
+        stack.push(new int[]{heights[0],-1});
+        ans[0] = -1;
+
+        for(int i=0;i<heights.length;i++){
             while(!stack.isEmpty() && heights[i] <= stack.peek()[0]){
                 stack.pop();
             }
 
             if(stack.isEmpty()){
-                arr[i] = -1;
+                  ans[i] = -1;
             }else{
-                arr[i] = stack.peek()[1];
+                  ans[i] = stack.peek()[1];
             }
-
-            stack.push(new int[]{heights[i], i});
+             stack.push(new int[]{heights[i],i});
         }
-
-        return arr;
+        return ans;
     }
 
-    public int[] NearestSmallerToRight(int[] heights){
-        int[] arr = new int[heights.length];
+    public int[] NSR(int[] heights){
+        int[] ans = new int[heights.length];
         Stack<int[]> stack = new Stack<>();
-        arr[heights.length - 1] = heights.length;
 
-        stack.push(new int[]{heights[heights.length - 1],heights.length-1});
+        stack.push(new int[]{heights[heights.length - 1],-1});
+        ans[heights.length - 1] = heights.length;
 
         for(int i=heights.length - 2;i>=0;i--){
             while(!stack.isEmpty() && heights[i] <= stack.peek()[0]){
@@ -40,40 +39,38 @@ class Solution {
             }
 
             if(stack.isEmpty()){
-                arr[i] = heights.length;
+                  ans[i] = heights.length;
             }else{
-                arr[i] = stack.peek()[1];
+                  ans[i] = stack.peek()[1];
             }
-
-            stack.push(new int[]{heights[i], i});
-        }
-
-        return arr;    
+             stack.push(new int[]{heights[i],i});
+        }        
+    return ans;
     }
-
 
     public int[] width(int[] left,int[] right){
         int[] width = new int[left.length];
-        for(int i=0;i<width.length;i++){
+        for(int i=0;i<left.length;i++){
             width[i] = right[i] - left[i] - 1;
         }
         return width;
     }
 
-    private int[] allAreas(int[] arr, int[] width){
-        int[] areas = new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            areas[i] = arr[i] * width[i];
-        }
-        return areas;
-    }
 
+    public int[] allAreas(int[] heights,int[] width){
+        int[] allArea = new int[heights.length];
+        for(int i=0;i<heights.length-1;i++){
+            allArea[i] = heights[i] * width[i];
+        }
+        return allArea;
+    }
 
     public int maxArea(int[] arr){
-        int max = 0;
-        for(int i : arr){
-            max = Math.max(i,max);
+        int area = 0;
+        for(int x : arr){
+            area = Math.max(x,area);
         }
-        return max;
+        return area;
     }
+
 }
