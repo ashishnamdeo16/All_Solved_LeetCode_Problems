bdf26f61b4ec1e965229494f12cb2cf59724cb00
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-05-06 18:48:48 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length == 0) return 0;
-        int maxProfit=0;
-        int minPrice=prices[0];
-        for(int i=1;i<prices.length;i++){
-            if(minPrice > prices[i]){
-                minPrice = prices[i];
-            }
-            int profit = prices[i] - minPrice;
-            if(profit > maxProfit){
-                maxProfit = profit;
+        int maxProfit = 0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                int profit = prices[j] - prices[i];
+                if(profit > 0){
+                    maxProfit = Math.max(maxProfit,profit);
+                }
             }
         }
         return maxProfit;
