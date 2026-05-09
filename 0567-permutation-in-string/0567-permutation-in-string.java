08ce7ec166774123de8062623abbad103fc60dc3
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-05-08 17:25:03 -0700

    Time: 6 ms (87.15%), Space: 43.7 MB (93.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+       if(s1.length() > s2.length()) return false;
        int[] freq = new int[26];
        int[] arr = new int[26];
        for(char s : s1.toCharArray()){
