3158d8103050c868ec92e4bccaa35c9e6e70c87b
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-06-17 21:12:43 -0700

    Time: 2 ms (99.39%), Space: 47.4 MB (11.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
         HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
@@ -8,28 +7,16 @@ class Solution {
             if(map.containsKey(sum)){
                 return new int[]{i,map.get(sum)};
             }
+
             map.put(nums[i],i);
         }
 
-        // HashMap<Integer,Integer> map = new HashMap<>();
-
-        // for(int i =0;i<nums.length;i++){
-        //     int sum = target - nums[i];
-        //     if(map.containsKey(sum)){
-        //         return new int[]{i,map.get(sum)};
-        //     }
-        //     map.put(nums[i],i);
-        // }
-
-        //Brute Force Solution
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i] + nums[j] == target){
-        //              return new int[]{i,j};
-        //         }
-        //     }
-        // }
-
-        return new int[]{-1,-1};
+        return  new int[]{-1,-1};
     }
 }
+
+//sum = 9 - 2 = 7
+// store 2 in map at index 0
+// sum = 9 - 7 = 2 
+// ye return index 
+//if not continue
