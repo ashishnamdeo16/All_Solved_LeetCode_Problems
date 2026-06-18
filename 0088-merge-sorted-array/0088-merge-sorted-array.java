0b81ff4f7473ba23ca74fb7f2823897540c3351d
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-06-17 21:37:13 -0700

    Time: 0 ms (100%), Space: 43.9 MB (27.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,41 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int l = 0;
-        int j = 0;
-        int i = 0;
-        int[] merged = new int[nums1.length];
-        while(l < m && j < n){
-            if(nums1[l] > nums2[j]){
-                merged[i++] = nums2[j];
-                j++;
-            }else if(nums1[l] <= nums2[j]){
-                merged[i++] = nums1[l];
-                l++;
+        int left = 0;
+        int right =0;
+        int i =0;
+        int[] merged = new int[m+ n];
+
+        while(left < m && right < n){
+            if(nums1[left] < nums2[right]){
+                merged[i++] = nums1[left];
+                left++;
+            }else{
+                merged[i++] = nums2[right];
+                right++;
             }
         }
 
-        while(j<n){
-                merged[i++] = nums2[j];
-                j++;
+        while(left < m){
+            merged[i++] = nums1[left];
+            left++;
         }
 
-        while(l<m){
-                merged[i++] = nums1[l];
-                l++;
+        while(right < n){
+            merged[i++] = nums2[right];
+            right++;
         }
 
-        for (int k = 0; k < m + n; k++) nums1[k] = merged[k];
+        for(int k=0;k<m+n;k++){
+            nums1[k] = merged[k];
+        }
 
-
-    //   Brute Force
-    //     int l=0;
-    //     for(int i=0;i<m+n;i++){
-    //      if(i >= m){
-    //          nums1[i] = nums2[l];
-    //          l++;
-    //      }
-    //    }
-    //    Arrays.sort(nums1);
-    // return merged;
     }
 }
