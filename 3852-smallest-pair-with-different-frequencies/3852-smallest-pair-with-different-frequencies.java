a826e31cc55c5aa1d9e6f6bada5194f3a87753ea
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-06-21 11:41:22 -0700

    Time: 3 ms (77.75%), Space: 46.7 MB (74.73%) - LeetHub

---
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for(int x : nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+            min = Math.min(x,min);
+        }
+
+        int secMin = Integer.MAX_VALUE;
+        
+        for(int x : nums){
+            if(x == min){
+                continue;
+            }
+
+            if(min < x  && map.get(x) != map.get(min)){
+                secMin = Math.min(secMin,x);
+            }
+        }
+    
+        if (secMin == Integer.MAX_VALUE) {
+            return new int[]{-1, -1};
+        } 
+        return new int[]{min, secMin};
+    }
+}
+
+// We need to find a Pair in which we need to find smallest values whose frequency is differnt 
+
+// I will make map in which i will store the values with there frequencies 
+// After that i will make Priority Queue 
