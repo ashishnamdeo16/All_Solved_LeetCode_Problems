a322670f8717fdedcea7e4ee826641f74e358a82
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-06-27 13:50:53 -0700

    Time: 5 ms (45.75%), Space: 46.6 MB (43.19%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,36 +1,41 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            graph.add(new ArrayList<>());
-        }
+    ArrayList<List<Integer>> graph = new ArrayList<>();
 
-        for(int i=0;i<edges.length;i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        boolean[] visited = new boolean[n];
-        int count = 0;
-
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,graph,visited);
-                count++;
-            }
-        }
-        return count;
+    for(int i=0;i<n;i++){
+        graph.add(new ArrayList<>());
     }
 
-    public void dfs(int i,List<List<Integer>> graph,boolean[] visited){
-        visited[i] = true;
-        for(int x : graph.get(i)){
+    for(int[] x : edges ){
+        int u = x[0];
+        int v = x[1];
+
+        graph.get(u).add(v);
+        graph.get(v).add(u);
+    }
+
+    boolean[] visited = new boolean[n];
+
+    int count = 0;
+
+    for(int i=0;i<graph.size();i++){
+        if(!visited[i]){
+            dfs(graph,visited,i);
+            count++;
+        }
+    }
+
+    return count;
+    }
+    
+    public void dfs( List<List<Integer>> graph, boolean[] visited,int node){
+        visited[node] = true;
+
+        for(int x : graph.get(node)){
             if(!visited[x]){
-                dfs(x,graph,visited);
+                dfs(graph,visited,x);
             }
         }
-    }   
+        return;
+    }
 }
