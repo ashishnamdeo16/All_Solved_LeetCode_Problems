38148b9c2c40b4c66cdd346cc85938dab0b59dc8
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-02 12:36:37 -0700

    Time: 58 ms (23.85%), Space: 48 MB (39.85%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,45 +1,57 @@
 class Solution {
-    int[] px = {0,-1,0,1};
+    int[] px = {0,1,0,-1};
     int[] py = {1,0,-1,0};
-
     public int minimumEffortPath(int[][] heights) {
         int m = heights.length;
         int n = heights[0].length;
-        int[] src = new int[]{0,0};
-        int[][] distance = new int[m][n];
-        for (int[] row : distance) {
-         Arrays.fill(row, Integer.MAX_VALUE);
-        }
-        distance[0][0] = 0;
+       int[][] distance = new int[m][n];
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.diff - b.diff);
-        pq.offer(new Pair(0,src));
+       for(int i=0;i<m;i++){
+        Arrays.fill(distance[i],Integer.MAX_VALUE);
+       }
+
+        distance[0][0] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.dis - b.dis);
+        pq.offer(new Pair(0,new int[]{0,0}));
+
         while(!pq.isEmpty()){
-            Pair parent = pq.poll();
-            int maxdiff = parent.diff;
-            int[] node = parent.nodeCordinates;
+            Pair p = pq.poll();
+            int currNodeDis = p.dis;
+            int[] currNodeCoordinate = p.coordinates;
+            int cx = currNodeCoordinate[0];
+            int cy = currNodeCoordinate[1];
 
             for(int i=0;i<4;i++){
-                int ii = node[0] + px[i];
-                int jj = node[1] + py[i];
-                if(ii < 0 || jj< 0 || ii >= m || jj >= n) continue;
-                int newDistance = Math.max(maxdiff,Math.abs(heights[node[0]][node[1]] - heights[ii][jj]));
-                if(newDistance < distance[ii][jj]){
-                    distance[ii][jj] = newDistance;
-                    pq.offer(new Pair(newDistance,new int[]{ii,jj}));
+                int x = px[i] + cx;
+                int y = py[i] + cy;
+
+            if(isValid(x,y,m,n)){
+                int newDis = Math.max(currNodeDis,Math.abs(heights[cx][cy] - heights[x][y]));
+                if(newDis < distance[x][y]){
+                      distance[x][y] = newDis;
+                        pq.offer(new Pair(newDis,new int[]{x,y}));
                 }
             }
+            }
         }
-        return distance[m-1][n-1];
+                    return distance[m-1][n-1];
+
     }
 
     class Pair{
-        int diff ;
-        int[] nodeCordinates;
+        int dis;
+        int[] coordinates;
 
-        public Pair(int diff,int[] nodeCordinates){
-            this.diff = diff;
-            this.nodeCordinates = nodeCordinates;
+        public Pair(int dis,int[] coordinates){
+            this.dis = dis;
+            this.coordinates = coordinates;
         }
+    }
+
+    public boolean isValid(int i,int j,int m,int n){
+        if(i < 0 || j < 0 || i>=m || j >= n){
+            return false;
+        }
+        return true;
     }
 }
