b91fa421936afeaca973afaff27f0c67df399c2b
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-06 10:39:30 -0700

    Time: 5 ms (61.64%), Space: 44.5 MB (67.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       if(s.length() != t.length()) return false;
-       HashMap<Character,Integer> mapS = new HashMap<>();
-       HashMap<Character,Integer> mapT = new HashMap<>();
-
-        for(char x : s.toCharArray()){
-         mapS.put(x,mapS.getOrDefault(x,0) + 1);
-        } 
-
-        for(char y : t.toCharArray()){
-         mapT.put(y,mapT.getOrDefault(y,0) + 1);
-        } 
-
-        if(mapS.equals(mapT)) return true;
-
+            if (s.length() != t.length()) {
         return false;
+        }
+        int[] freq = new int[26];
+        int[] freq2 = new int[26];
 
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+            freq2[t.charAt(i) - 'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i] != freq2[i]){
+                return false;
+            }
+        }
+
+        return true;
+        
+        
+    //    if(s.length() != t.length()) return false;
+    //    HashMap<Character,Integer> map1 = new HashMap<>();
+    //    HashMap<Character,Integer> map2 = new HashMap<>();
+
+    //    for(char q : s.toCharArray()){
+    //         map1.put(q,map1.getOrDefault(q,0)+1);
+    //    }
+
+    //    for(char m : t.toCharArray()){
+    //         map2.put(m,map2.getOrDefault(m,0)+1);
+    //    }
+
+    //    return map1.equals(map2);
     }
 }
