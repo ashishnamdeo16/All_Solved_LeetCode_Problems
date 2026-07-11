17e4b8fad04f4c4102f1f6a46558e60177fc0ae9
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-11 16:17:25 -0700

    Time: 20 ms (65.63%), Space: 45.5 MB (94.86%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,33 +1,26 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        ArrayList<List<Integer>> finalAns = new ArrayList<>();
+        List<List<Integer>> finalList = new ArrayList<>();
         Arrays.sort(nums);
 
-       for(int i =0;i<nums.length;i++){
-            if(i > 0 && nums[i] == nums[ i - 1 ]) continue;
-
-            for(int j=i+1;j<nums.length;j++){
-                if(j > i + 1 && nums[j] == nums[ j - 1 ]) continue;
+        for(int i=0;i<nums.length;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1;j<nums.length;j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
                 int l = j+1;
-                int r = nums.length -1;
+                int r = nums.length - 1;
 
-                while(l<r){
-                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+                while(l < r){
+                    long sum = (long) nums[l] + nums[r] + nums[i] + nums[j];
 
                     if(sum == target){
-                        List<Integer> arr = new ArrayList<>();
-                        arr.add(nums[i]);
-                        arr.add(nums[j]);
-                        arr.add(nums[l]);
-                        arr.add(nums[r]);
+                        List<Integer> list = Arrays.asList(nums[l],nums[r],nums[i],nums[j]);
+                        finalList.add(list);
                         l++;
                         r--;
-                        finalAns.add(arr);
-
-                        while(l < r && nums[l] == nums[l-1]) l++;
-                        while(r >= l && nums[r] == nums[r+1]) r--;
-
+                        while(l<r && nums[r] == nums[r+1]) r--;
+                        while(l<r && nums[l] == nums[l-1]) l++;
                     }else if(sum > target){
                         r--;
                     }else{
@@ -35,8 +28,7 @@ class Solution {
                     }
                 }
             }
-       }
-
-        return finalAns;
+        }
+        return finalList;
     }
 }
