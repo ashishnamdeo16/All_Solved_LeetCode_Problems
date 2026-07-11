acaa5cabc05d989730c39980032209e44afad504
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-11 14:49:13 -0700

    Time: 2 ms (95.75%), Space: 48.6 MB (11.19%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l = 0;
-        int r = numbers.length - 1;
-        while(l < r){
+        int l=0;
+        int r=numbers.length-1;
+
+        while(l<r){
             int sum = numbers[l] + numbers[r];
             if(sum > target){
                 r--;
@@ -11,7 +12,11 @@ class Solution {
             }else{
                 return new int[]{l+1,r+1};
             }
+
         }
-        return new int[]{-1,-1};
+       return new int[]{-1,-1};
     }
 }
+
+//It can also be solved using hashmap but will create an additional overhead for keeping and maintaining the hashmap 
+//Hence we chose 2 pointer here to find since array is already sorted
