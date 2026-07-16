1cd71829be17dbb55587c01f5d216d58725a0922
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-16 14:49:12 -0700

    Time: 6 ms (70.34%), Space: 46.6 MB (20.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,34 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       HashMap<Character,Integer> map = new HashMap<>();
-       int l = 0;
-       int r = 0;
-       int len = Integer.MIN_VALUE;
-       StringBuilder sb = new StringBuilder(s);
+      HashSet<Character> set = new HashSet<>();
+      int len = 0;  
+      int l = 0;
+      int r = 0;
 
-       while(r < s.length()){
-            char ch = s.charAt(r);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+      while(r < s.length()){
+        char ch = s.charAt(r);
 
-            while(!isValid(map)){
-                map.put(s.charAt(l),map.get(s.charAt(l)) - 1);
-                if(map.get(s.charAt(l)) == 0){
-                    map.remove(s.charAt(l));
-                }
-                l++;
-            }
-
-            len = Math.max(len,r - l + 1);
-            r++;
-       }
-        return len == Integer.MIN_VALUE ? 0 : len;
-    }
-    public boolean isValid(HashMap<Character,Integer> map){
-        for(Map.Entry<Character,Integer> entry : map.entrySet()){
-            if(entry.getValue() > 1){
-                return false;
-            }
+        while(set.contains(ch)){
+            set.remove(s.charAt(l));
+            l++;
         }
-        return true;
+        
+        len = Math.max(len,r - l + 1);
+
+        set.add(ch);
+        r++;
+      } 
+
+      return len;
     }
 }
