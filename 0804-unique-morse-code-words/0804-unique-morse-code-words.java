1af112ee464fec747c3af137cda11bf3de043c15
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-07-23 12:25:24 -0700

    Time: 4 ms (10.24%), Space: 43.6 MB (13.04%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> set = new HashSet<>();
+        HashMap<Character,String> mp = new HashMap<>();
+        mp.put('a', ".-");
+        mp.put('b', "-...");
+        mp.put('c', "-.-.");
+        mp.put('d', "-..");
+        mp.put('e', ".");
+        mp.put('f', "..-.");
+        mp.put('g', "--.");
+        mp.put('h', "....");
+        mp.put('i', "..");
+        mp.put('j', ".---");
+        mp.put('k', "-.-");
+        mp.put('l', ".-..");
+        mp.put('m', "--");
+        mp.put('n', "-.");
+        mp.put('o', "---");
+        mp.put('p', ".--.");
+        mp.put('q', "--.-");
+        mp.put('r', ".-.");
+        mp.put('s', "...");
+        mp.put('t', "-");
+        mp.put('u', "..-");
+        mp.put('v', "...-");
+        mp.put('w', ".--");
+        mp.put('x', "-..-");
+        mp.put('y', "-.--");
+        mp.put('z', "--..");
+
+        for(String x : words){
+            String st = "";
+            for(char ch : x.toCharArray()){
+                 st += mp.get(ch);
+            }
+            set.add(st);
+        }
+
+        return set.size();
+    }
+}
