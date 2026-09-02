06b586a98bae9a169a32fa6089da63863dc3c9a5
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-09-02 12:57:06 -0700

    Time: 33 ms (19.81%), Space: 45.5 MB (82.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) { 
-        int[] needed = new int[256];
-        int[] have = new int[256];
+        int[] freqT = new int[128];
+        int[] freqS = new int[128];
         int minlen = Integer.MAX_VALUE;
         int start = 0;
 
-        for(char x : t.toCharArray()){
-            needed[x]++;
+        for(char ch : t.toCharArray()){
+            freqT[ch]++;
         }
+        
         int l = 0;
-        int r =0;
-        char[] arr = s.toCharArray();
+        int r = 0;
 
-        while(r < arr.length){
-              have[arr[r]]++;
-              while(isValid(have,needed)){
-                 have[arr[l]]--;
-                 int len = r - l + 1;
-                 if(minlen > len){
+        while(r < s.length()){
+            char ch = s.charAt(r);
+            freqS[ch]++;
+
+            while(isValid(freqS,freqT)){
+                freqS[s.charAt(l)]--;
+                
+                int len = r - l + 1;
+
+                if(minlen > len){
                     minlen = len;
                     start = l;
-                 }
-                 l++;
-             }
-             r++;   
+                }
+                l++;
+            }
+
+            r++;
         }
+
+
         return minlen == Integer.MAX_VALUE ? "" : s.substring(start,start+minlen);
+        
     }
 
-
-    public boolean isValid(int[] have, int [] needed){
-        for(int i = 0;i<needed.length;i++){
-            if(have[i] < needed[i]){
+    public boolean isValid( int[] freqS , int[] freqT ){
+        for(int i =0;i<freqS.length;i++){
+            if(freqT[i] > freqS[i]){
                 return false;
             }
         }
+
         return true;
     }
 }
