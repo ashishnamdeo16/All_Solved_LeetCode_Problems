f0ec9db596d6db41e52b54625cb589e525c34775
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-09-02 14:17:59 -0700

    Time: 18 ms (33.64%), Space: 46.9 MB (46.01%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,53 +1,46 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        HashMap<Character,Integer> pMap = new HashMap<>();
-        HashMap<Character,Integer> sMap = new HashMap<>();
-
-        for(char c : p.toCharArray()){
-            pMap.put(c,pMap.getOrDefault(c,0)+1);
-        }
+        List<Integer> arr = new ArrayList<>();
 
         int l = 0;
         int r = 0;
-        List<Integer> arr = new ArrayList<>();
 
-        while(r<s.length()){
-            char ch = s.charAt(r);
-            sMap.put(ch,sMap.getOrDefault(ch,0)+1);
+        int[] freqP = new int[128];
+        int[] freqS = new int[128];
 
-            // Maintain window size equal to p length
-            if (r - l + 1 > p.length()) {
-                char leftChar = s.charAt(l);
-                sMap.put(leftChar, sMap.get(leftChar) - 1);
-
-                if (sMap.get(leftChar) == 0) {
-                    sMap.remove(leftChar);
-                }
-
-                l++;
-            }
-
-            // Check current window
-            if (r - l + 1 == p.length() && isValid(pMap, sMap)) {
-                arr.add(l);
-            }
-
-            r++;
+        for(char ch :p.toCharArray()){
+            freqP[ch]++;
         }
+
+        int k = p.length();
+
+        while(r < s.length()){
+            freqS[s.charAt(r)]++;
+
+            if(r - l + 1 < k){
+                r++;
+            }else{
+                if(isValid(freqP,freqS)){
+                    arr.add(l);
+                }
+                freqS[s.charAt(l)]--;
+                l++;
+                r++;
+            }
+        }
+
 
         return arr;
     }
 
-    public boolean isValid(Map<Character,Integer> pMap,Map<Character,Integer> sMap){
-        if(pMap.size() != sMap.size()){
-            return false;
-        }
-        for(Map.Entry<Character,Integer> entry : sMap.entrySet()){
-            if(!pMap.containsKey(entry.getKey()) || !pMap.get(entry.getKey()).equals(entry.getValue())){
+    public boolean isValid(int[] freqP,int[] freqS){
+        for(int i =0 ;i<freqS.length;i++){
+            if(freqS[i] != freqP[i]){
                 return false;
             }
         }
-
         return true;
     }
+
+
 }
