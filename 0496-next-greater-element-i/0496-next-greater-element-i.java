5b39f1246987b22df8322ced20b60d1c2590c28b
Ashish Namdeo <ashishnamdev14@gmail.com>
2026-09-06 14:49:42 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
-        int n = nums2.length;
-        int[] arr = new int[n];
-        int[] arr2 = new int[nums1.length];
-        stack.push(nums2[n - 1]);
-        arr[n-1] = -1;
-        for(int i=nums2.length - 2;i>=0;i--){
-            while(!stack.isEmpty() && nums2[i] >= stack.peek()){
+        int[] arr = new int[nums2.length];
+        int[] farr = new int[nums1.length];
+
+        arr[nums2.length - 1] = -1;
+        stack.push(arr[nums2.length - 1]);
+
+        for(int i = nums2.length - 2 ;i >= 0;i--){
+
+            while(!stack.isEmpty() && stack.peek() <= arr[i]){
                 stack.pop();
             }
 
@@ -20,15 +22,21 @@ class Solution {
             stack.push(nums2[i]);
         }
 
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
+        for(int i =0;i< nums1.length;i++){
+            for(int j =0;j<nums2.length;j++){
                 if(nums1[i] == nums2[j]){
-                    arr2[i] = arr[j];
+                    farr[i] = arr[j];
                     break;
                 }
             }
         }
 
-        return arr2;
+
+        return farr;
+
+
+
     }
 }
+
+//We can optimize it by using hashMap
